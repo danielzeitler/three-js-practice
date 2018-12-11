76e5201ee9d712f146a8ce75f553958ebe4e1c83
Daniel Zeitler <dz@danielzeitler.de>
2018-12-11 18:29:18 +0100

    feat: add bouncing ball animation

--- 02_Ball-Animation/app.js
+++ 02_Ball-Animation/app.js
@@ -63,9 +63,16 @@ function init() {
 
     document.querySelector("#ball-animation").appendChild(renderer.domElement)
 
+    let step = 0;
     function renderScene() {
         stats.update()
 
+        // Let the ball bounce!
+        step += 0.04
+        sphere.position.x = 20 + 10*(Math.cos(step))
+        sphere.position.y = 2 + 10*Math.abs(Math.sin(step))
+
+        // Rotate Cube
         cube.rotation.x += 0.02
         cube.rotation.y += 0.02
         cube.rotation.z += 0.02
