14461f8a2c6e42a5faa7d6c9207235e75d6ad138
Daniel Zeitler <dz@danielzeitler.de>
2018-12-13 17:04:06 +0100

    feat: add Rubiks Cube like animation

---
+++ 05_Materials/app.js
@@ -0,0 +1,75 @@
+let scene;
+let camera;
+let renderer;
+
+function init() {
+    let stats = initStats()
+
+    scene = new THREE.Scene()
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 1000)
+    renderer = new THREE.WebGLRenderer()
+
+    renderer.shadowMap.enabled = true
+    renderer.setClearColor(new THREE.Color(0x000000))
+    renderer.setSize(window.innerWidth, window.innerHeight)
+
+    // Helper for the x, y, z axes
+    let axesHelper = new THREE.AxesHelper(20)
+    scene.add(axesHelper)
+
+    let planeGeometry = new THREE.PlaneGeometry(60, 40, 1, 1)
+    let planeMesh = new THREE.MeshLambertMaterial({
+        color: 0xCCCCCC
+    })
+    let plane = new THREE.Mesh(planeGeometry, planeMesh)
+    plane.rotation.x = -0.5 * Math.PI
+    plane.position.set(0, 0, 0)
+    plane.receiveShadow = true
+    scene.add(plane)
+
+    // Materials
+    let sphereGeo = new THREE.SphereGeometry(5, 20, 20)
+    let sphereMaterial = new THREE.MeshNormalMaterial({flatShading: false})
+    let sphere = new THREE.Mesh(sphereGeo, sphereMaterial)
+    sphere.position.set(0, 7, 0)
+    scene.add(sphere)
+    console.log(sphereMaterial.uuid)
+
+    // add Spotlight
+    let spotLight = new THREE.SpotLight(0xFFFFFF, /* 1.2, 150, 120 */)
+    spotLight.position.set(-40, 60, -10)
+    spotLight.castShadow = true
+    scene.add(spotLight)
+
+    // subtle ambient lighting
+    let ambienLight = new THREE.AmbientLight(0x3c3c3c)
+    scene.add(ambienLight)
+
+    camera.position.set(-35, 15, 30)
+    camera.lookAt(scene.position)
+
+    document.querySelector('#cube-scene').appendChild(renderer.domElement)
+
+    // Adjust scene on browser resize
+    window.addEventListener('resize', () => {
+        camera.aspect = window.innerWidth / window.innerHeight
+        camera.updateProjectionMatrix()
+        renderer.setSize(window.innerWidth, window.innerHeight)
+        renderer.render(camera, scene)
+    })
+    
+    // add TrackballControls
+    let trackballControls = initTrackballControls(camera, renderer)
+    let clock = new THREE.Clock()
+
+    // Render scene with animations
+    function renderScene() {
+        stats.update()
+        trackballControls.update(clock.getDelta())
+
+        requestAnimationFrame(renderScene)
+        renderer.render(scene, camera)
+    }
+
+    renderScene()
+}
---
+++ 06_RubiksCube/app.js
@@ -0,0 +1,82 @@
+function init() {
+    let scene = new THREE.Scene()
+    let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+    let renderer = new THREE.WebGLRenderer()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+
+    let axesHelper = new THREE.AxesHelper(20)
+    scene.add(axesHelper)
+
+    let group = new THREE.Mesh()
+    let mats = []
+
+    mats.push(new THREE.MeshLambertMaterial({color: 0x00FFFF}))
+    mats.push(new THREE.MeshLambertMaterial({color: 0xFF00FF}))
+    mats.push(new THREE.MeshLambertMaterial({color: 0xFFFF00}))
+    mats.push(new THREE.MeshLambertMaterial({color: 0xFF0000}))
+    mats.push(new THREE.MeshLambertMaterial({color: 0x00FF00}))
+    mats.push(new THREE.MeshLambertMaterial({color: 0x0000FF}))
+
+    for(let x = 0; x < 3; x++) {
+        for(let y = 0; y < 3; y++) {
+            for(let z = 0; z < 3; z++) {
+                let cubeGeom = new THREE.BoxGeometry(2.9, 2.9, 2.9)
+                let cube = new THREE.Mesh(cubeGeom, mats)
+                cube.position.set(x * 5 - 5, y * 5 - 5, z * 5 - 5)
+
+                group.add(cube)
+            }
+        }
+    }
+
+    group.scale.copy(new THREE.Vector3(2,2,2))
+    scene.add(group)
+
+
+    camera.position.set(0, 80, 80)
+    camera.lookAt(scene.position)
+
+    // Init Light
+    initLight(scene)
+
+    window.addEventListener("resize", () => {
+        camera.aspect = window.innerWidth / window.innerHeight
+        camera.updateProjectionMatrix()
+    
+        renderer.setSize(window.innerWidth, window.innerHeight)
+        renderer.render(scene, camera)
+    })
+
+    document.querySelector("#rubiks-scene").appendChild(renderer.domElement)
+    
+    let trackBallControls = initTrackballControls(camera, renderer)
+    let clock = new THREE.Clock()
+
+    function render() {
+        trackBallControls.update(clock.getDelta())
+
+        group.rotation.x += 0.01
+        group.rotation.y += 0.01
+        group.rotation.z += 0.01
+
+        requestAnimationFrame(render)
+        renderer.render(scene, camera)
+    }
+
+    render()
+}
+
+function initLight(scene) {
+    let ambientLight = new THREE.AmbientLight(0x3c3c3c)
+    scene.add(ambientLight)
+
+    let spotLight = new THREE.SpotLight(0xFFFFFF)
+    spotLight.position.set(35, 30, 0)
+
+    let spotLightHelper = new THREE.SpotLightHelper(spotLight)
+    // scene.add(spotLightHelper)
+
+    scene.add(spotLight)
+}
+
